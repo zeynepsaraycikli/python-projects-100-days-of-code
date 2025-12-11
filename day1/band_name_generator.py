238cbf178d4cdb315b4785756a2c3690bfd78ff5
zeynep saraycıklı <zeynepsaraycikli@zeynep-MacBook-Pro-3.local>
2025-12-11 18:27:49 +0300

    band name generator

---
+++ day1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator")
+city = input("what is the name of the city you grew up in?\n")
+pet = input("what is the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
