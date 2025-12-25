8ee8e9873cea2c079f9a9332d34c36b6c2422d4c
zeynep saraycıklı <zeynepsaraycikli@zeynep-MacBook-Pro-3.local>
2025-12-26 00:04:53 +0300

    day8 eklendi

---
+++ day8-caesar cipher/main.py
@@ -0,0 +1,27 @@
+import art
+
+print (art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    for letter in original_text:
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+
+
